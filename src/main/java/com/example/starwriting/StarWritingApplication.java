6e87412c0d558c0e6bdde90f053dc7146546152f
unwind <ysh038@naver.com>
2022-12-13 15:51:28 +0900

    init commit

---
+++ src/main/java/com/example/starwriting/StarWritingApplication.java
@@ -0,0 +1,13 @@
+package com.example.starwriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWritingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWritingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/starwriting/StarWritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starwriting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWritingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
