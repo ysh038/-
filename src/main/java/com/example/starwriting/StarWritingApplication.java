71d87ba9a25239057ddb227cbb21e43811ddbf9c
unwind <ysh038@naver.com>
2022-12-13 16:26:08 +0900

    test

--- src/main/java/com/example/starwriting/StarWritingApplication.java
+++ src/main/java/com/example/starwriting/StarWritingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StarWritingApplication {
-
+// wntjr
     public static void main(String[] args) {
         SpringApplication.run(StarWritingApplication.class, args);
     }
